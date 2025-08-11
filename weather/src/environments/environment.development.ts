91c8deda5a1eaf5aa9468a1c4cf907ddb08e9778
daviddprtma <davidchristianpratama@gmail.com>
2025-08-11 12:03:59 +0700

    Add environment files to .gitignore and remove from index

--- weather/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    weatherApiKey: 'ee65e5301b2d4839aeb7951f35843cd2'
-};
--- weather/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  weatherApiKey: 'ee65e5301b2d4839aeb7951f35843cd2',
-};
